fd2da56179c1709790290046cfa2e0bc2eec9a25
Jérôme D.Soucy <jerome.duguay-soucy@workjam.com>
2021-06-16 16:07:25 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "Jerome D.Soucy",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "275952140",
+      },
+    },
+  ],
+};
